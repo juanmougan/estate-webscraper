86fd319de45184a3b5e872be508db43eabfcb1d6
Juan Manuel Mougan <juanmougan@gmail.com>
2019-07-13 00:04:33 -0300

    Basic loop

--- web_scraper.rb
+++ web_scraper.rb
@@ -2,13 +2,17 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
+# TODO should receive document structure from somewhere instead. Maybe a YAML file?
 def parse_params
-  raise ArgumentError, 'Missing base URL' unless ARGV.size == 1
-  return ARGV[0]
+  raise ArgumentError, 'Missing base URL or search results CSS class' unless ARGV.size == 2
+  return ARGV[0], ARGV[1]
 end
 
-url = parse_params
+url, search_results = parse_params
 html = open(url)
 
 doc = Nokogiri::HTML(html)
-puts doc
+doc.css(".#{search_results}").each do |item|
+  # TODO parse everything here. Bear in mind pagination!
+  puts item
+end
