1277509bc5c94e5cc049a961492ceb87e8ee4036
Juan Manuel Mougan <juanmougan@gmail.com>
2019-07-25 00:08:09 -0300

    Adding functionality as part of String

--- web_scraper.rb
+++ web_scraper.rb
@@ -2,6 +2,13 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
+# Adding some parsing functionality directly into String
+class String
+  def delete_dollar_sign
+    self&.strip&.delete_prefix("$")&.strip
+  end
+end
+
 # TODO should receive document structure from somewhere instead. Maybe a YAML file?
 def parse_params
   raise ArgumentError, 'Missing base URL or search results CSS class or 3 CSS classes' unless ARGV.size == 5
@@ -19,9 +26,9 @@ doc.css(".#{search_results}").each do |item|				# TODO extract CSS to some confi
   puts '<!-- End of item -->'
   address = item.at_css(".#{address_css}").text.strip
   puts "Address: #{address}"
-  price = item.at_css(".#{price_css}")&.text&.strip&.delete_prefix("$")&.strip&.tr('.', '')
+  price = item.at_css(".#{price_css}")&.text&.delete_dollar_sign&.tr('.', '')
   # TODO refactor this!
-  expenses = item.at_css(".#{expenses_css}")&.text&.strip&.delete_prefix("&amp;plus;")&.strip&.delete_prefix("&plus;")&.strip&.delete_prefix("$")&.strip&.delete_suffix("expensas")&.strip&.tr('.', '')
+  expenses = item.at_css(".#{expenses_css}")&.text&.strip&.delete_prefix("&amp;plus;")&.strip&.delete_prefix("&plus;")&.delete_dollar_sign&.delete_suffix("expensas")&.strip&.tr('.', '')
   puts "Price: #{price}"
   puts "Expenses: #{expenses}"
 end
